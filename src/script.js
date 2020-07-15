9c2df8d5e41cdca5d4925a49dc605bca9f8a81b9
Ryan Jones <ryguy243@outlook.com>
2020-07-15 02:40:54 -0700

    set up scatter plots and began tooltip

--- src/script.js
+++ src/script.js
@@ -5,6 +5,12 @@ const w = 800;
 const h = 500;
 const padding = 60;
 
+const tooltip = d3
+  .select('.chart')
+  .append('div')
+  .attr('id', 'tooltip')
+  .style('opacity', 0);
+
 d3.json(
   'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json'
 ).then(function (data) {
@@ -15,7 +21,7 @@ d3.json(
     minutes.push(currentItem.Time);
   });
   const time = [years, minutes];
-d
+
   // Parse time
   const specifier = '%M:%S';
   const parsedData = minutes.map((d) => d3.timeParse(specifier)(d));
@@ -30,13 +36,13 @@ d
   const xScale = d3
     .scaleLinear()
     .domain([d3.min(years) - 1, d3.max(years) + 1])
-    .range([0, w]);
+    .range([padding, w - padding]);
 
   // yScale
   const yScale = d3
     .scaleTime()
     .domain(d3.extent(parsedData))
-    .range([h - padding, padding]);
+    .range([padding, h - padding]);
 
   // SVG
   d3.select('svg')
@@ -45,16 +51,29 @@ d
     .enter()
     .append('circle')
     .attr('class', 'dot')
-    .attr('cx', (d) => xScale(d.Year))
-    .attr('cy', (d) => yScale(d.Time))
-    .attr('r', (d) => 5);
+    .attr('cx', (d, i) => xScale(years[i]))
+    .attr('cy', (d, i) => yScale(parsedData[i]))
+    .attr('r', (d) => 5)
+    .attr('data-xvalue', (d, i) => years[i])
+    .attr('data-yvalue', (d, i) => parsedData[i])
+    .on('mouseover', (d, i) => {
+      tooltip.transition().duration(200).style('opacity', 0.9);
+      tooltip
+        .html(`hello`)
+        .attr('data-year', years[i])
+        .style('left', `${xScale(years[i]) + 57}px`)
+        .style('top', `${yScale(parsedData[i]) + 40}px`);
+    })
+    .on('mouseout', () => {
+      tooltip.transition().duration(200).style('opacity', 0);
+    });
 
   // Bottom axis
   const xAxis = d3.axisBottom(xScale).tickFormat(d3.format('R'));
   d3.select('svg')
     .append('g')
     .call(xAxis)
-    .attr('transform', 'translate(60, 440)')
+    .attr('transform', 'translate(0, 440)')
     .attr('id', 'x-axis');
 
   // Left axis
